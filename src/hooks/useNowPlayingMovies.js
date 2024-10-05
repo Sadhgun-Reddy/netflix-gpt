4e87f0bcacb1e868b77ef9c5423e6311a258bb91
Sadhgun-Reddy <sadhgunreddy09@gmail.com>
2024-10-05 22:56:57 +0530

    Till Main Container.

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -5,21 +5,20 @@ import { useEffect } from "react";
 
 const useNowPlayingMovies =()=>{
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const getNowPlayingMovies = async () =>{
-      const data = await fetch(
-        'https://api.themoviedb.org/3/movie/now_playing?page=1',
-        API_OPTIONS
-      );
-      const json = await data.json();
-      console.log(json.results);
-      dispatch(addNowPlayingMovies(json.results));
-    };
-  
-    useEffect(() => {
-      getNowPlayingMovies();
-    }, []);
+  const getNowPlayingMovies = async () =>{
+    const data = await fetch(
+      'https://api.themoviedb.org/3/movie/now_playing?page=1',
+      API_OPTIONS
+    );
+    const json = await data.json();
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
 };
 
 export default useNowPlayingMovies;
